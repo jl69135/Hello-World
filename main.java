c5cc6315669763689d8c32caba00e7bf5abeff20
jl69135 <69662401+jl69135@users.noreply.github.com>
2020-08-13 20:55:56 -0700

    Add files via upload
    my 1st java program

---
+++ main.java
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World");
+		System.out.println("Watching Korean Dramas");
+		System.out.println("Minecraft");
+		System.out.println("UC Davis");
+		System.out.println("Economics");
+		System.out.println("I love music");
+	}
+}
